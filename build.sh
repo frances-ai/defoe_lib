dfeb2b90c46821bb1247dad51fb0c73b00742f53
Wilfrid Askins <wilfridaskins@gmail.com>
2022-08-08 11:09:40 +0100

    Update build.sh with requirements parameter

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
+
+requirements=$1
+if [[ -z "$requirements" ]] ; then
+  requirements="requirements.txt"
+fi
+
 python3 -m venv defoe_env
 source defoe_env/bin/activate
-pip3 install -r requirements.txt
+pip3 install -r $requirements
 venv-pack -o defoe_env.tar.gz
-
