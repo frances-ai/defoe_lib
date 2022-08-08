3bf807fb430737b66052c58ce1a8927dee970a64
Wilfrid Askins <wilfridaskins@gmail.com>
2022-08-08 12:41:10 +0100

    Add zip step to build file

--- build.sh
+++ build.sh
@@ -9,3 +9,9 @@ python3 -m venv defoe_env
 source defoe_env/bin/activate
 pip3 install -r $requirements
 venv-pack -o defoe_env.tar.gz
+
+targetDir=$2
+if [[ -z "$targetDir" ]] ; then
+  targetDir="./defoe_lib"
+fi
+zip -r defoe_lib.zip $targetDir
